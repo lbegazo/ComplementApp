7260c11856ac9d190a797c76b71a493348465572
lbegazo <luis_begazo@hotmail.es>
2020-06-01 18:06:13 -0500

    Initial commit

---
+++ ComplementApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using ComplementApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComplementApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        
+         public DbSet<Value> Values { get; set; }   
+        
+    }
+}
