ee5fe852238249191bcc4c02817fe001f4d19a58
lbegazo <luis_begazo@hotmail.es>
2020-06-22 09:11:22 -0500

    End of 3rd party components

--- ComplementApp-SPA/src/app/nav/nav.component.ts
+++ ComplementApp-SPA/src/app/nav/nav.component.ts
@@ -1,38 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  login()
-  {
+  login() {
     this.authService.login(this.model).subscribe(
-        next => { console.log('Logged  in successfully'); },
-        error => { console.log(error);
-      });
+      (next) => {
+        this.alertify.success('Logged  in successfully');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-  loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
-  logout()
-  {
+  logout() {
     localStorage.removeItem('token');
-    console.log('Logged out!!');
-
+    this.alertify.message('Logged out!!');
   }
-
 }
--- ComplementApp-SPA/src/app/register/register.component.ts
+++ ComplementApp-SPA/src/app/register/register.component.ts
@@ -1,35 +1,28 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  //@Input() valuesFromHome: any;
   @Output() cancelRegisterEvent = new EventEmitter();
   model: any = {};
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private alertify: AlertifyService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onRegister()
-  {
+  onRegister() {
     this.auth.register(this.model).subscribe(
-      () => console.log('Registration successful'),
-      error => console.log(error)
+      () => this.alertify.success('Registration successful'),
+      (error) => this.alertify.error(error)
     );
   }
 
-  onCancel()
-  {
+  onCancel() {
     this.cancelRegisterEvent.emit(false);
-    console.log('Cancelled');
+    this.alertify.error('Cancelled');
   }
-
-  
-
 }
